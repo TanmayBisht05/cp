cf15ac6663b9c988b1d7b75a8c7cf765482d324f
TanmayBisht05 <tanmaybisht2005@gmail.com>
2025-04-03 02:42:57 +0530

    3april2025

---
+++ binary_exponentiation.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+// -------------------------------------------------------------------------
+
+
+ll binexp(ll a, ll b, ll mod){
+    ll ans=1;
+    while(b){
+        if(b&1) ans=(ans*a)%mod;
+        a=(a*a)%mod;
+        b>>=1;
+    }
+    return ans;
+}
+
+
+
+
+// ----------------------------------------------------------------------------
+
+
+
+
+
+
+
+void solve() {
+    
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+return 0;
+}
---
+++ boilerplate.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+void solve() {
+    
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while (t--) {
+        solve();
+    }
+return 0;
+}
